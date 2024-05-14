3221a2c7b4c0e6206edd8513ad5ec39a7eb6b337
mikeGugs <michaelrguglielmo@gmail.com>
2024-05-13 20:37:01 -0400

    update requirements

--- instance/config.py
+++ instance/config.py
@@ -1,11 +1,8 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-print(basedir)
 
 DEBUG = True
-SECRET_KEY = 'hard to guess string'
-#SECRET_KEY = '2zTEjfiUXwY4rffFvJ7w0NiR3E6Plt'
+SECRET_KEY = '2zTEjfiUXwY4rffFvJ7w0NiR3E6Plt'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database.db')
-print(SQLALCHEMY_DATABASE_URI)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
